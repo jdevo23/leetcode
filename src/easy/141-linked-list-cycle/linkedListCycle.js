1d515eab09eef619f18ca5e2908472cbabcb5e9b
unknown <jrdeveson@hotmail.com>
2022-04-21 13:52:54 +0100

    replace lets with consts

--- src/easy/141-linked-list-cycle/linkedListCycle.js
+++ src/easy/141-linked-list-cycle/linkedListCycle.js
@@ -27,24 +27,24 @@ function LinkedList(head = null) {
   this.head = head;
 }
 
-let node1 = new ListNode(3);
-let node2 = new ListNode(2);
-let node3 = new ListNode(0);
-let node4 = new ListNode(-4);
+const node1 = new ListNode(3);
+const node2 = new ListNode(2);
+const node3 = new ListNode(0);
+const node4 = new ListNode(-4);
 node1.next = node2;
 node2.next = node3;
 node3.next = node4;
 node4.next = node2;
-let list1 = new LinkedList(node1);
+const list1 = new LinkedList(node1);
 
-let node5 = new ListNode(1);
-let node6 = new ListNode(2);
+const node5 = new ListNode(1);
+const node6 = new ListNode(2);
 node5.next = node6;
 node6.next = node5;
-let list2 = new LinkedList(node5);
+const list2 = new LinkedList(node5);
 
-let node7 = new ListNode(1);
-let list3 = new LinkedList(node7);
+const node7 = new ListNode(1);
+const list3 = new LinkedList(node7);
 
 console.log(hasCycle(node1));   // output == true
 console.log(hasCycle(node5));   // output == true

79469e5ba299f67e342a89f9ad596f1ab56bfca4
unknown <jrdeveson@hotmail.com>
2022-04-27 12:09:12 +0100

    delete import, move indentation

--- src/easy/145-binary-tree-postorder-traversal/binaryTreePostorderTraversal.py
+++ src/easy/145-binary-tree-postorder-traversal/binaryTreePostorderTraversal.py
@@ -1,42 +1,41 @@
-from tkinter.tix import Tree
 from typing import Optional, List
 
 class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+  def __init__(self, val=0, left=None, right=None):
+    self.val = val
+    self.left = left
+    self.right = right
 
 class Solution:
-    def postorderTraversalRecursive(self, root: Optional[TreeNode]) -> List[int]:
-      list = []
+  def postorderTraversalRecursive(self, root: Optional[TreeNode]) -> List[int]:
+    list = []
 
-      def recurse(self, node: Optional[TreeNode]) -> None:
-        if (node == None):
-          return
-        if (node.left):
-          recurse(self, node.left)
-        if (node.right):
-          recurse(self, node.right)
+    def recurse(self, node: Optional[TreeNode]) -> None:
+      if (node == None):
+        return
+      if (node.left):
+        recurse(self, node.left)
+      if (node.right):
+        recurse(self, node.right)
 
-        list.append(node.val)
+      list.append(node.val)
 
-      recurse(self, root)
-      return list
-      
-    def postorderTraversalIterative(self, root: Optional[TreeNode]) -> List[int]:
-      if (root == None):
-        return []
+    recurse(self, root)
+    return list
+    
+  def postorderTraversalIterative(self, root: Optional[TreeNode]) -> List[int]:
+    if (root == None):
+      return []
 
-      stack1, stack2 = [root], []
+    stack1, stack2 = [root], []
 
-      while (stack1):
-        root = stack1.pop()
-        if (root):
-          stack2.insert(0, root.val)
-        if (root.left):
-          stack1.append(root.left)
-        if (root.right):
-          stack1.append(root.right)
-      
-      return stack2
+    while (stack1):
+      root = stack1.pop()
+      if (root):
+        stack2.insert(0, root.val)
+      if (root.left):
+        stack1.append(root.left)
+      if (root.right):
+        stack1.append(root.right)
+    
+    return stack2

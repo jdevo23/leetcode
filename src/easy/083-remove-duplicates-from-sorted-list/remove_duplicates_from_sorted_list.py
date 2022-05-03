ff43e16ce5d4a971848ece9ab6447e2e93844571
unknown <jrdeveson@hotmail.com>
2022-05-03 17:03:42 +0100

    linting, file name change

--- src/easy/083-remove-duplicates-from-sorted-list/remove_duplicates_from_sorted_list.py
+++ src/easy/083-remove-duplicates-from-sorted-list/remove_duplicates_from_sorted_list.py
@@ -1,23 +1,25 @@
 from typing import Optional
 
+
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 
+
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        map = {}
+        hashmap = {}
 
         current = head
         prev = ListNode(-1, current)
 
-        while (current):
-          if (current.val in map):
-            prev.next = current.next
-          else:
-            map[current.val] = current.val
-            prev = prev.next
-          current = current.next
-        
+        while current:
+            if current.val in hashmap:
+                prev.next = current.next
+            else:
+                hashmap[current.val] = current.val
+                prev = prev.next
+            current = current.next
+
         return head

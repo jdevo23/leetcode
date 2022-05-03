2abcc409f1db8a50db729ee2cac5030dd37e854c
unknown <jrdeveson@hotmail.com>
2022-05-03 17:01:46 +0100

    linting, file name change

--- src/easy/009-palindrome-number/palindrome_number.py
+++ src/easy/009-palindrome-number/palindrome_number.py
@@ -1,10 +1,10 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         x, i = list(str(x)), 0
-        
-        for y in x:
-            if y != x[len(x) - 1 - i]:
+
+        for _y in x:
+            if _y != x[len(x) - 1 - i]:
                 return False
-            i+=1
-            
+            i += 1
+
         return True

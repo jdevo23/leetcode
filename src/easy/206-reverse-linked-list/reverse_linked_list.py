da1972ef37106f9d8d41f429cc35d6c402c1fad4
unknown <jrdeveson@hotmail.com>
2022-05-03 17:04:44 +0100

    linting, file name change

--- src/easy/206-reverse-linked-list/reverse_linked_list.py
+++ src/easy/206-reverse-linked-list/reverse_linked_list.py
@@ -1,20 +1,22 @@
 from typing import Optional
 
+
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 
+
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         prev = None
         curr = head
-        next = None
+        next_node = None
 
-        while (curr != None):
-          next = curr.next
-          curr.next = prev
-          prev = curr
-          curr = next
-        
+        while curr is not None:
+            next_node = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_node
+
         return prev

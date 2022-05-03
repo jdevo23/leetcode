8853ea6f3b74238b45f15f34b669fac24dcc3951
unknown <jrdeveson@hotmail.com>
2022-05-03 17:05:58 +0100

    linting, file name change

--- src/medium/102-binary-tree-level-order-traversal/binary_tree_level_order_traversal.py
+++ src/medium/102-binary-tree-level-order-traversal/binary_tree_level_order_traversal.py
@@ -1,32 +1,35 @@
 from typing import Optional, List
 
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 
+
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         if not root:
-          return []
-        
+            return []
+
         level, results = [root], []
 
         while level:
-          temp_level, temp_results = [], []
-          
-          for node in level:
-            temp_results.append(node.val)
-            if node.left:
-              temp_level.append(node.left)
-            if node.right:
-              temp_level.append(node.right)
-          
-          results.append(temp_results)
-          level = temp_level
-        
+            temp_level, temp_results = [], []
+
+            for node in level:
+                temp_results.append(node.val)
+                if node.left:
+                    temp_level.append(node.left)
+                if node.right:
+                    temp_level.append(node.right)
+
+            results.append(temp_results)
+            level = temp_level
+
         return results
+
 
 testSolution = Solution()
 

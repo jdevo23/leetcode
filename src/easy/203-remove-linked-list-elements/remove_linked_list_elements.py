bceadd3d8043626af6932338ce65a684124761ab
unknown <jrdeveson@hotmail.com>
2022-05-03 17:04:40 +0100

    linting, file name change

---
+++ src/easy/203-remove-linked-list-elements/remove_linked_list_elements.py
@@ -0,0 +1,26 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        if head is None:
+            return head
+
+        while (head is not None and head.val == val):
+            head = head.next
+
+        current = head
+
+        while current and current.next:
+            if current.next.val == val:
+                current.next = current.next.next
+            else:
+                current = current.next
+
+        return head

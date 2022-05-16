4122ff3a4930235e7da2b3f70df456057e0d4ad7
unknown <jrdeveson@hotmail.com>
2022-05-16 16:49:54 +0100

    add solution (278: First Bad Version)

---
+++ src/easy/278-first-bad-version/first_bad_version.py
@@ -0,0 +1,17 @@
+# The isBadVersion API is already defined for you.
+def isBadVersion(version: int) -> bool:
+
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        left, right = 1, n
+
+        while left < right:
+            m = (left + right) // 2
+
+            if isBadVersion(m):
+                right = m
+            else:
+                left = m + 1
+        
+        return left

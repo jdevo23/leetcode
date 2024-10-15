8e7a20a8e413eabd704e5ec05d0d4887177c950e
Jonny <jrdeveson@hotmail.com>
2024-10-15 17:32:33 +0100

    update answer to use o(log n) time complexity

--- src/easy/035-search-insert-position/searchInsertPosition.ts
+++ src/easy/035-search-insert-position/searchInsertPosition.ts
@@ -1,14 +1,25 @@
 function searchInsert(nums: number[], target: number): number {
-    let index = nums.length;
+    let num = 0;
+    let i = 0;
+    let j = nums.length;
+    let mid = Math.floor(nums.length / 2);
 
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] >= target ) {
-            index = i;
+    while (i <= mid && j >= mid) {
+        if (nums[i] >= target) {
+            num = i;
             break;
         }
+
+        if (nums[j - 1] <= target) {
+            num = nums[j - 1] < target ? j : j - 1;
+            break;
+        }
+
+        i++;
+        j--;
     }
 
-    return index;
+    return num;
 };
 
 
@@ -28,6 +39,21 @@ const tests = [
         nums: [1,3,5,6],
         target: 7,
         output: 4,
+    },
+    {
+        nums: [1],
+        target: 2,
+        output: 1,
+    },
+    {
+        nums: [1,3],
+        target: 3,
+        output: 1,
+    },
+    {
+        nums: [1,3,5],
+        target: 4,
+        output: 2,
     },
 ];
 
